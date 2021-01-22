ab5d0ab06246e2826076d7382b075203ecdeae26
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-01-22 23:20:45 +0100

    complete the bubble_sort and modify readme file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,23 @@
 bubble (arr)= [12,34,22,42,50]
 
 def bubble_sort #defining the method
-    array_length =array.size
-    return arr if list.size <= 1 #sorting
-    swapped = true
+    array_length = array.size
+    return array if array_length <= 1 #sorting
+    swapped = false
     while swapped do #iteration
-        if #conditional statement & loop?
+         #conditional statement & loop?
+    #iterating throught the array
+    (array_length-1).times do |i|
+        if array[i] > array[i+1]
+            array[i], array[i+1] = array[i+1], array[i]
+            swapped = true
+        end
+    end
+
+        break if not swapped
+end
+
+array
+end
+
 
--- tests.rb
+++
@@ -1,5 +0,0 @@
-arr = [2,5,1,7,3,8]
-
-arr.size.times do |i|
-    p i
-end
