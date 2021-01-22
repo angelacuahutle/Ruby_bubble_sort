9a74eb2e3a7d2f08e62876266e944f59a64a53f9
angelacuahutle <angela.cuahutle@gmail.com>
2021-01-22 14:02:21 -0600

    dedfined methodand return

---
+++ bubble_sort.rb
@@ -0,0 +1,9 @@
+bubble (arr)= [12,34,22,42,50]
+
+def bubble_sort #defining the method
+    array_length =array.size
+    return arr if list.size <= 1 #sorting
+    swapped = true
+    while swapped do #iteration
+        if #conditional statement & loop?
+
---
+++ tests.rb
@@ -0,0 +1,5 @@
+arr = [2,5,1,7,3,8]
+
+arr.size.times do |i|
+    p i
+end
