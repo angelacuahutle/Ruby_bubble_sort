40291831cdc7a13568420ab8a4422cdaf330b6c2
angelacuahutle <angela.cuahutle@gmail.com>
2021-01-22 20:04:33 -0600

    stil fix the test

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,23 @@
-bubble (arr)= [12,34,22,42,50]
-
-def bubble_sort #defining the method
-    array_length = array.size
-    return array if array_length <= 1 #sorting
+array = [12,34,22,42,50]
+# create a method taking an array as its argument
+def bubble_sort(array) #defining the method
+    array_length = array.size #create a variable that has the length of the array
+    return array if array_length <= 1 #avoid redundance
     swapped = false
     while swapped do #iteration
-         #conditional statement & loop?
-    #iterating throught the array
-    (array_length-1).times do |i|
+         #conditional statement & loop
+         #iterating throught the array
+        (array_length-1).times do |i| #substracting one makes the array works
         if array[i] > array[i+1]
             array[i], array[i+1] = array[i+1], array[i]
-            swapped = true
+            swapped = true  # use if statement that verify values [i] and [i+1]
+           # and swap if value [i]>[i+1]
         end
     end
-
-        break if not swapped
+    break if not swapped
+  end
 end
+puts bubble_sort(array)
 
-array
-end
 
 
